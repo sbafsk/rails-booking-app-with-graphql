d59928f54bda4d96c1fd762882c1f5a16f8a6b7f
sbafsk <sebastian@occupier.com>
2021-11-04 18:58:39 -0300

    routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
-Rails.application.routes.draw do
-  resources :users
-  resources :bookings
-  resources :rooms
+# frozen_string_literal: true
 
-  
+Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      get 'bookings/index'
+      post 'booking/create'
+      delete 'booking/:id', to: 'booking#destroy'
+    end
+  end
+
+  root 'bookings#index'
+  # resources :users
+  # resources :bookings
+  # resources :rooms
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
