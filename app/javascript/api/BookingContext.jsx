4578d0ac2b2c79cde30cff06f8fe24eb845a0063
sbafsk <sebastian@occupier.com>
2021-11-22 21:56:52 -0300

    addboking, udded types

--- app/javascript/api/BookingContext.jsx
+++ app/javascript/api/BookingContext.jsx
@@ -1,6 +1,11 @@
-import React, { useReducer, useContext, useCallback } from "react"
+import React, { useReducer, useContext, useCallback, useEffect } from "react"
 
-import { REQUEST_BOOKINGS, SET_LOADING, ADD_BOOKING } from "./action_types"
+const REQUEST_BOOKINGS = "REQUEST_BOOKINGS"
+const ADD_BOOKING = "ADD_BOOKING"
+const SORT_BOOKINGS = "SORT_BOOKINGS"
+const SET_LOADING = "SET_LOADING"
+
+const url = "api/v1/bookings/"
 
 const BookingContext = React.createContext()
 
@@ -18,21 +23,27 @@ const reducer = (state, action) => {
   switch (type) {
     case REQUEST_BOOKINGS:
       return {
-        ...state,
         bookings: payload,
         loading: false
       }
     case ADD_BOOKING:
       return {
+        bookings: { ...state.bookings, payload },
+        loading: false
+      }
+    case SORT_BOOKINGS:
+      debugger
+      const filterBookings = state.bookings.map((b) => b.day === payload.day)
+      console.log(filterBookings)
+      return {
         ...state,
-        bookings: { ...state.bookings, payload }
+        bookings: filterBookings
       }
     case SET_LOADING:
       return {
         ...state,
         loading: payload
       }
-
     default:
       break
   }
@@ -43,9 +54,9 @@ export const BookingProvider = ({ children }) => {
 
   const getBookings = useCallback(async () => {
     dispatch({ type: SET_LOADING, payload: true })
-    const url = "api/v1/bookings/index"
+
     try {
-      const response = await fetch(url)
+      const response = await fetch(`${url}index`)
       if (!response.ok) throw new Error("Network error.")
       const data = await response.json()
       const bookings = data.map((b) => {
@@ -56,25 +67,54 @@ export const BookingProvider = ({ children }) => {
           dateTo: b.date_to,
           user: b.user_name,
           mail: b.user_mail,
-          room: b.room_id
+          room: b.room_id,
+          show: true
         }
       })
       dispatch({ type: REQUEST_BOOKINGS, payload: bookings })
     } catch (error) {
       console.log(error)
-      setError(true)
+      return false
     }
   }, [])
 
-  const addBooking = (booking) => {
+  const addBooking = async (booking) => {
     console.log("adding booking", booking)
+    dispatch({ type: SET_LOADING, payload: true })
+    try {
+      const response = await fetch(`${url}create`, {
+        method: "POST",
+        body: JSON.stringify(booking),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      if (!response.ok) throw new Error("Network error.")
+      const data = await response.json()
+      console.log(data)
+      debugger
+      dispatch({ type: ADD_BOOKING, payload: booking })
+    } catch (error) {
+      console.log(error)
+      return false
+    }
   }
+
+  const filterBookingsByDay = (day) => {
+    console.log(day)
+    dispatch({ type: "FILTER_BOOKINGS", payload: day })
+  }
+
+  useEffect(() => {
+    getBookings()
+  }, [getBookings])
 
   const value = {
     bookings: state.bookings,
     isLoading: state.loading,
     getBookings,
-    addBooking
+    addBooking,
+    filterBookingsByDay
   }
 
   return (
--- app/javascript/api/action_types.js
+++
@@ -1,5 +0,0 @@
-export const REQUEST_BOOKINGS = "REQUEST_BOOKINGS"
-
-export const ADD_BOOKING = "ADD_BOOKING"
-
-export const SET_LOADING = "SET_LOADING"
