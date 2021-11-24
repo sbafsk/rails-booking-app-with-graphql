6b892403ff20e6f5f864d9d6f8f715912e85f52c
sbafsk <sebastian@occupier.com>
2021-11-24 19:52:22 -0300

    commented types

--- app/javascript/types/index.ts
+++ app/javascript/types/index.ts
@@ -1,3 +1,11 @@
+/**
+ * @param id?
+ * @param from
+ * @param to
+ * @param user_name
+ * @param user_mail
+ * @param room
+ */
 export interface IBooking {
   id?: number
   from: Date
@@ -7,6 +15,14 @@ export interface IBooking {
   room: string
 }
 
+/**
+ * @param day
+ * @param fromTime
+ * @param toTime
+ * @param name
+ * @param email
+ * @param room
+ */
 export interface IBookingForm {
   day: string
   fromTime: string
@@ -16,11 +32,30 @@ export interface IBookingForm {
   room: string
 }
 
+/**
+ * @param IBooking[]
+ * @param loading
+ * @param selectedDay
+ * @param error
+ * @param dialog
+ */
 export interface IBookingState {
   bookings: IBooking[]
   loading: boolean
   selectedDay: Date
   error: null | string
+  dialog: IDialogProps
+}
+
+/**
+ * @param open
+ * @param title
+ * @param message
+ */
+export interface IDialogProps {
+  open: boolean
+  title: string
+  message: string
 }
 
 export enum BookingActions {
@@ -28,5 +63,6 @@ export enum BookingActions {
   ADD_BOOKING = "ADD_BOOKING",
   FILTER_BOOKINGS = "FILTER_BOOKINGS",
   SET_LOADING = "SET_LOADING",
-  SET_ERROR = "SET_ERROR"
+  SET_ERROR = "SET_ERROR",
+  SET_DIALOG = "SET_DIALOG"
 }
