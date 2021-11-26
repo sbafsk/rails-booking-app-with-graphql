6ee016c3bec965ed8868d96d48f6164a438578a3
sbafsk <sebastian@occupier.com>
2021-11-26 15:08:57 -0300

    model validation on progress...

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,6 +5,13 @@ class Booking < ApplicationRecord
   validates :from, :to, :user_name, :user_mail, :room, presence: true
   # check date validations
 
+  validate :period_not_taken
 
+  def period_not_taken
+    # consultar si los periodos no se overlap
+
+    # if error > errors.add(attr, msg)
+
+  end
 
 end
