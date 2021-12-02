09d4da117b3b172992af17150d64feb893d9ae13
sbafsk <sebastian@occupier.com>
2021-12-02 17:21:14 -0300

    booking test

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -57,4 +57,17 @@ class BookingTest < ActiveSupport::TestCase
 
     assert !new_booking.valid?
   end
+
+  test 'when booking is containing a saved book' do
+
+    new_booking = Booking.new({
+                                from: DateTime.parse('02 Dec 9:30AM'),
+                                to: DateTime.parse('02 Dec 11:30AM'),
+                                user_name: 'seba',
+                                user_mail: 'test@test.com',
+                                room: 'sala-1'
+                              })
+
+    assert !new_booking.valid?
+  end
 end
