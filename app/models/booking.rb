95c022a3beb99bae09cfa3b9e2535548cf64f42a
sbafsk <sebastian@occupier.com>
2021-12-02 17:21:46 -0300

    created model validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
-class Booking < ApplicationRecord
+# frozen_string_literal: true
 
+# testing class Booking
+class Booking < ApplicationRecord
   # way to manage props with something like type interface.
 
   validates :from, :to, :user_name, :user_mail, :room, presence: true
@@ -8,9 +10,16 @@ class Booking < ApplicationRecord
   validate :period_not_taken
 
   def period_not_taken
-    # consultar si los periodos no se overlap
+    has_from_between_records =
+      Booking
+      .where('? between bookings.from and bookings.to and ? != bookings.to', from, from)
+      .any?
+    has_to_between_records =
+      Booking
+      .where('? between bookings.from and bookings.to and ? != bookings.from', to, to)
+      .any?
 
-    # if error > errors.add(attr, msg)
+    return errors.add(:from, :to, message: 'From or To dates are between some record.') if has_from_between_records || has_to_between_records
 
   end
 
