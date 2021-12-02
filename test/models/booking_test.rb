304ee131e88076f7ca4d8599b8734ce4e733b0c3
sbafsk <sebastian@occupier.com>
2021-12-02 17:05:18 -0300

    test for booking validations created

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,7 +1,60 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class BookingTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @existing_booking = Booking.create({
+                                         from: DateTime.parse('02 Dec 10AM'),
+                                         to: DateTime.parse('02 Dec 11AM'),
+                                         user_name: 'seba',
+                                         user_mail: 'test@test.com',
+                                         room: 'sala-1'
+                                       })
+  end
+
+  teardown do
+    @existing_booking.destroy
+  end
+
+  test 'when booking is not intersected' do
+    new_booking = Booking.new({
+                                from: DateTime.parse('02 Dec 8AM'),
+                                to: DateTime.parse('02 Dec 10AM'),
+                                user_name: 'seba',
+                                user_mail: 'test@test.com',
+                                room: 'sala-1'
+                              })
+
+    assert new_booking.valid?
+  end
+
+  test 'when booking is partial intersected' do
+    new_booking = Booking.new({
+                                from: DateTime.parse('02 Dec 9AM'),
+                                to: DateTime.parse('02 Dec 10:30AM'),
+                                user_name: 'seba',
+                                user_mail: 'test@test.com',
+                                room: 'sala-1'
+                              })
+
+    assert !new_booking.valid?
+  end
+
+  test 'when booking is contained' do
+
+    new_booking = Booking.new({
+                                from: DateTime.parse('02 Dec 10:30AM'),
+                                to: DateTime.parse('02 Dec 11AM'),
+                                user_name: 'seba',
+                                user_mail: 'test@test.com',
+                                room: 'sala-1'
+                              })
+
+    assert !new_booking.valid?
+  end
 end
