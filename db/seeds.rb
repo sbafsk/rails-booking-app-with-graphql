a0cf2c82f9d60f8c3e675949dc6679a2a3f3ac64
sbafsk <sebastian@occupier.com>
2021-12-02 17:20:57 -0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,12 +8,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-booking1 = Booking.create({
-  from: DateTime.new(2021, 11, 8, 10, 0, 0),
-  to: DateTime.new(2021, 11, 8, 12, 0, 0),
-  user_name: 'seba',
-  user_mail: 'test@test.com',
-  room: 'sala-1'
-})
-
+Booking.create([{
+                 from: DateTime.parse('2021 Dec 8 10'),
+                 to: DateTime.parse('2021 Dec 8 11'),
+                 user_name: 'seba',
+                 user_mail: 'test@test.com',
+                 room: 'sala-1'
+               },
+                {
+                  from: DateTime.parse('2021 Dec 8 14'),
+                  to: DateTime.parse('2021 Dec 8 16'),
+                  user_name: 'seba',
+                  user_mail: 'test@test.com',
+                  room: 'sala-1'
+                },
+                {
+                  from: DateTime.parse('2021 Dec 8 18'),
+                  to: DateTime.parse('2021 Dec 8 19'),
+                  user_name: 'seba',
+                  user_mail: 'test@test.com',
+                  room: 'sala-1'
+                }])
