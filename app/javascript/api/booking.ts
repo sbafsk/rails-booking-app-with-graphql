4a5c92683a1a7fd026498562b6d4c8f46f8f104e
sbafsk <sebastian@occupier.com>
2021-12-03 18:49:28 -0300

    csrf token validation

--- app/javascript/api/booking.ts
+++ app/javascript/api/booking.ts
@@ -4,6 +4,9 @@ import { IBooking } from "../types"
 
 const url = "api/v1/bookings/"
 
+const getToken = () =>
+  document.querySelector('meta[name="csrf-token"]').getAttribute("content")
+
 export const getBookings = async (): Promise<IBooking[]> => {
   const response = await fetch(`${url}index`)
   if (!response.ok) throw new Error("Error al obtener las reservas.")
@@ -12,13 +15,15 @@ export const getBookings = async (): Promise<IBooking[]> => {
 }
 
 export const postBooking = async (booking: IBooking) => {
+  const token = getToken()
   const response = await fetch(`${url}create`, {
     method: "POST",
     body: JSON.stringify(
       mapKeys(booking, (val, key) => (key = snakeCase(key)))
     ),
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "X-CSRF-TOKEN": token
     }
   })
   if (!response.ok) throw new Error("Error al crear la reserva.")
