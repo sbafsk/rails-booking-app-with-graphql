bcf57f2eaf572149198f5b8dee8fab885154a839
sbafsk <sebastian@occupier.com>
2021-12-03 16:11:38 -0300

    controller bug fix

--- app/controllers/react_controller.rb
+++ app/controllers/react_controller.rb
@@ -2,12 +2,6 @@
 
 # React Controller
 class ReactController < ApplicationController
-  # need to disable the CSRF token checking that Rails automatically configures for our apps.
-  # avoid having to validate the tokens each time we perform a request.
-  # ???
-
-  protect_from_forgery with: :null_session
-
   layout 'react'
 
   def index
